c425f1e3f81030095f37ed7533e90d88a647cbc1
devLeSilverio <leticia.silverio09@gmail.com>
2020-10-25 06:35:08 +0900

    atualizando

--- www/components/acao.js
+++ www/components/acao.js
@@ -11,7 +11,7 @@ var json= {cont:contador-1};
 var  teste= json.cont;
 console.log(teste);
 
-   $(".principal").append("<div class='row'><div class='col-2 mt-2'><div class='form-group'><img src='lib/icone_telefone.png' id='iconTelefone' style='width:50px; height:50px;'></div></div><div class='col-6 mt-2'><label id='nome' >"+$("#nn").val()+"</label><label id='numero' >"+$("#nt").val()+"</label><label id='email' >"+$("#ne").val()+"</label></div><div class='col-2 mt-2'><div class='form-group'><button class="+teste+" class='btn btn-danger btn-lg' id='delete'><img src='lib/icon_delete.png' id='imgEdit' width='20px' height='20px'/></button></div></div><div class='col-2 mt-2'><div class='form-group'><button class='btn  btn-info btn-lg' id='edit'><img src='lib/icon_edit.png' id='imgEdit' width='20px' height='20px'/></button></div></div></div>"); 
+   $(".principal").append("<div class='row'><div class='col-2 mt-2'><div class='form-group'><img src='lib/icone_telefone.png' id='iconTelefone' style='width:50px; height:50px;'></div></div><div class='col-6 mt-2'><label id='nome' >"+$("#nn").val()+"</label><label id='numero' >"+$("#nt").val()+"</label><label id='email' >"+$("#ne").val()+"</label></div><div class='col-2 mt-2'><div class='form-group'><button class='btn btn-danger btn-lg' id='delete'><img src='lib/icon_delete.png' id='imgEdit' width='20px' height='20px'/></button></div></div><div class='col-2 mt-2'><div class='form-group'><button class='btn  btn-info btn-lg' id='edit'><img src='lib/icon_edit.png' id='imgEdit' width='20px' height='20px'/></button></div></div></div>"); 
 
 clear();
 });
@@ -19,10 +19,9 @@ clear();
 testando = 1;
 
 $(document).on("click","."+testando,function(){
- 
+ localStorage.setItem("a","a");
  testando++;
 });
-
 
 
 const clear = ()=>{
